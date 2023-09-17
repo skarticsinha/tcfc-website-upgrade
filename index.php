f4c3a2a95a685bdf57f53ccc74cde585ba219b70
skarticsinha <skarticsinha@gmail.com>
2023-09-18 00:28:49 +0530

    added document head
    Added document head from the orignal index.html file.

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+// This is the start of the HTML document
+?>
+
+<html>
+
+<head>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-QH5Y3F8FSX"></script>
+	<script>
+		// Initialize Google Analytics (gtag.js)
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		// Configure Google Analytics with your ID
+		gtag('config', 'G-QH5Y3F8FSX');
+	</script>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;700&display=swap" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="css/aos.css" type="text/css">
+	<link rel="stylesheet" href="css/slider-style.css">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+	<title>TCFC | Bespoke Furniture Manufacturer in Dubai and UAE</title>
+	<meta name="description" content="Looking for a Bespoke furniture manufacturer in Dubai? TCFC build premium custom furniture for individuals, interior designers, and businesses. Call Us Now!" />
+	<meta name="google-site-verification" content="K7RgfkLHZ9LH0EzNd-iAY-eu4_T44UWkhdA_Y-ZBteI" />
+	<link rel="canonical" href="https://tcfc.ae/" />
+</head>
