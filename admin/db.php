0f518dce7876e3e1ef94b7570c5e0bdb4a728089
skarticsinha <skarticsinha@gmail.com>
2023-09-28 16:54:38 +0530

    created database connecteion

---
+++ admin/db.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";  // Replace with your server name or IP address
+$username = "tcfcDBmanager"; // Replace with your database username
+$password = "}rwYFP\$6t\$j64)M"; // Replace with your database password
+$dbname = "tcfcDB";   // Replace with your database name
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
