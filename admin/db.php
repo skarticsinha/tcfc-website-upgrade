2243c77df915ca0d4867594ebbf2f751cd1c9e25
skarticsinha <skarticsinha@gmail.com>
2023-09-30 07:36:40 +0530

    Fixed DB connection

--- admin/db.php
+++ admin/db.php
@@ -7,8 +7,9 @@ $dbname = "tcfcDB";   // Replace with your database name
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check the connection
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
-?>
+  }
+    echo "Connected successfully";
+  ?>    
